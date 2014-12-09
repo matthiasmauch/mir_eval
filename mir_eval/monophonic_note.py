303ec2ed43ab69f11e4bb68f78d4ec7659286b30
Matthias Mauch <m.mauch@qmul.ac.uk>
2014-12-09 15:43:07 +0000

    implemented basic rasterizing function

--- mir_eval/monophonic_note.py
+++ mir_eval/monophonic_note.py
@@ -11,17 +11,81 @@ import warnings
 from . import util
 
 
+def remove_zero_notes(interval, value):
+    '''
+    Takes a sequence of notes given by the time interval (in seconds) and
+    a value, and removes those with 0 value.
+
+    :parameters:
+    - interval : np.ndarray, shape=(n_events, 2)
+        Onset and offset time of each note.
+    - value : np.ndarray shape=(n_events, 1) (or list of values)
+        Array of pitches or frequencies.
+
+    :returns:
+    - outinterval : np.ndarray, shape=(n_events, 2)
+        Onset and offset time of each note.
+    - outvalue : np.ndarray shape=(n_events, 1)
+        Array of pitches or frequencies.
+
+    '''
+
+    value = np.array(value)
+    outinterval = interval[value>0,:]
+    outvalue = value[value>0]
+    return outinterval, outvalue
+
+
+def rasterize_notes(interval, value, hopsize=0.01):
+    '''
+    Takes a sequence of notes given by the time interval (in seconds) and
+    a value and turns them into a single list of frame-wise values sampled
+    at hopsize intervals.
+
+    :usage:
+        TODO
+
+    :parameters:
+        - interval : np.ndarray, shape=(n_events, 2)
+            Onset and offset time of each note
+        - value : np.ndarray shape=(n_events, 1) (or list of values)
+            Array of pitches or frequencies
+        - hopsize : float
+            Period in seconds at which to sample the notes
+
+    :returns:
+        - rasterized_notes : np.ndarray, shape=(n_events, 1)
+            Note values sampled at a regular hopsize intervals.
+
+    '''
+
+    interval, value = remove_zero_notes(interval, value)
+
+    n_note = len(value)
+    n_frame = n_note / hopsize + 1
+    t = np.arange(n_frame) * hopsize
+
+    rasterized_notes = np.zeros((n_note, 1))
+
+    for i_note in range(n_note):
+        index = np.logical_and(t >= inverval[i_note, 1],  
+                               t <  inverval[i_note, 2])
+        if max(f0[index]) > 0:
+            raise # overlapping notes!
+        else:
+            rasterized_notes[index] = value[i_note]
+    return rasterized_notes
+
+
 def evaluate(ref_interval, ref_midi, est_interval, est_midi, **kwargs):
     '''
     Evaluate two note transcriptions, where the first is treated as the
     reference (ground truth) and the second as the estimate to be evaluated
     (prediction).
 
-    :usage: TODO
-        >>> ref_time, ref_freq = mir_eval.io.load_time_series('ref.txt')
-        >>> est_time, est_freq = mir_eval.io.load_time_series('est.txt')
-        >>> scores = mir_eval.melody.evaluate(ref_time, ref_freq,
-                                              est_time, est_freq)
+    :usage: 
+        TODO
+
     :parameters:
         - ref_interval : np.ndarray, shape=(n_events, 2)
             Onset and offset time of each reference note
@@ -40,22 +104,22 @@ def evaluate(ref_interval, ref_midi, est_interval, est_midi, **kwargs):
             Dictionary of scores, where the key is the metric name (str) and
             the value is the (float) score achieved.
 
-    :raises:
-        - ValueError
-            Thrown when the provided annotations are not valid.
     '''
+
+    ref_vector = rasterized_notes(ref_interval, ref_midi)
+    est_vector = rasterized_notes(est_interval, est_midi)
 
     # Compute metrics
     scores = collections.OrderedDict()
 
-    (scores['Voicing Recall'],
-     scores['Voicing False Alarm']) = util.filter_kwargs(voicing_measures,
-                                                         ref_voicing,
-                                                         est_voicing, **kwargs)
+    # (scores['Voicing Recall'],
+    #  scores['Voicing False Alarm']) = util.filter_kwargs(voicing_measures,
+    #                                                      ref_voicing,
+    #                                                      est_voicing, **kwargs)
 
-    scores['Raw Pitch Accuracy'] = util.filter_kwargs(raw_pitch_accuracy,
-                                                      ref_voicing, ref_cent,
-                                                      est_voicing, est_cent,
-                                                      **kwargs)
+    # scores['Raw Pitch Accuracy'] = util.filter_kwargs(raw_pitch_accuracy,
+    #                                                   ref_voicing, ref_cent,
+    #                                                   est_voicing, est_cent,
+    #                                                   **kwargs)
 
     return scores
